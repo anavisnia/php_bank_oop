cec980bd8f12e97ce6aebae47404b6e6f534c437
Ana Višnevskaja <anavisnia@gmail.com>
2021-03-12 15:47:46 +0200

    Json.php contruct and distruct functions

--- app/Json.php
+++ app/Json.php
@@ -1,5 +1,26 @@
 <?php
 
 class Json {
+    private $data;
+    private static $jsonObject;
+
+    public static function getDB()
+    {
+        return self::$jsonObject ?? self::$jsonObject = new Self;
+    }
     
+    public function __construct()
+    {
+        if(!file_exists(DIR.'data/users.json')) {
+            $data = json_encode([]);
+            file_put_contents(DIR.'data/users.json', $data);
+        }
+        $data = file_get_contents(DIR.'data/users.json');
+        $this->$data = json_decode($data);
+    }
+
+    public function __destruct()
+    {
+        file_put_contents(DIR.'data/users.json', json_decode($this->data));
+    }
 }

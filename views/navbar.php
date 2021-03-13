a439a44fb9428cdaca86e8ff307b396be7634dd4
Ana Višnevskaja <anavisnia@gmail.com>
2021-03-13 12:16:41 +0200

    basic user creation

--- views/navbar.php
+++ views/navbar.php
@@ -1,6 +1,6 @@
 <div class="topnav">
     <a class="active" href="<?= URL?>">Pagrindinis</a>
-    <a href="<?= URL?>create.php">Sukurti nauja saskaita</a>
-    <a href="<?= URL?>add.php">Prideti lesas</a>
-    <a href="<?= URL?>withdraw.php">Nuskaiciuoti lesas</a>
+    <a href="<?= URL?>create">Sukurti nauja saskaita</a>
+    <a href="<?= URL?>add">Prideti lesas</a>
+    <a href="<?= URL?>withdraw">Nuskaiciuoti lesas</a>
 </div>

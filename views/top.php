8a70be93dd6aecaabbda5291ea6c356587e52360
Ana Višnevskaja <anavisnia@gmail.com>
2021-03-15 10:23:58 +0200

    css fix

--- views/top.php
+++ views/top.php
@@ -4,9 +4,12 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="<?= URL.'components/css/reset.css'?>">
-    <link rel="stylesheet" href="<?= URL.'components/css/nav.css'?>">
-    <link rel="stylesheet" href="<?= URL.'components/css/main.css'?>">
+    <link rel="stylesheet" href="<?= URL?>components/css/reset.css">
+    <link rel="stylesheet" href="<?= URL?>components/css/nav.css">
+    <link rel="stylesheet" href="<?= URL?>components/css/create.css">
+    <link rel="stylesheet" href="<?= URL?>components/css/add.css">
+    <link rel="stylesheet" href="<?= URL?>components/css/withdraw.css">
+    <link rel="stylesheet" href="<?= URL?>components/css/main.css">
     <title>Bankas</title>
 </head>
 <body>
